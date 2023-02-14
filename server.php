44702f7dc4689b7ebaa3a0e0fca2f8a3c60e8740
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2023-02-14 04:54:28 +0300

    feat: accept webroot argument

--- server.php
+++ server.php
@@ -3,8 +3,9 @@
 if (PHP_SAPI !== 'cli-server') {
     die('this is only for the php development server');
 }
-$fileFullPath = getcwd().'/webroot'.$_SERVER['SCRIPT_NAME'];
+$webroot = $argv[1] ?? 'webroot';
 
+$fileFullPath = getcwd() . '/' .  $webroot . $_SERVER['SCRIPT_NAME'];
 if (is_file($fileFullPath)) {
     header('Content-Type: '. mime_content_type($fileFullPath));
     die(file_get_contents($fileFullPath));
@@ -12,4 +13,4 @@ if (is_file($fileFullPath)) {
 
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 // require the entry point
-require 'webroot/index.php';
+require $webroot . '/index.php';
