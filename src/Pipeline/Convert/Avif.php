6aa5512f76ae9504bddd16dcf7632d702384734c
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2023-02-18 02:12:05 +0300

    fix: avif default quality issue

--- src/Pipeline/Convert/Avif.php
+++ src/Pipeline/Convert/Avif.php
@@ -16,7 +16,7 @@ class Avif implements Converter
         }
         $newSourceFile = str_contains($sourceFile, '.avif') ? $sourceFile : $sourceFile.'.avif';
         $image = $this->createImageFromFilename($sourceFile);
-        imageavif($image, $newSourceFile, 0, -1);
+        imageavif($image, $newSourceFile, 65, 10);
         if ($newSourceFile !== $sourceFile) {
             unlink($sourceFile);
         }
