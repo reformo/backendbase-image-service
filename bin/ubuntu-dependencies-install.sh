021a6aa03efcbf847a00590e5f0692704332b9cf
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2023-02-21 01:23:16 +0300

    feat: jpeg support for imagemagick added

--- bin/ubuntu-dependencies-install.sh
+++ bin/ubuntu-dependencies-install.sh
@@ -1,11 +1,11 @@
-apt-get install build-essential webp pngquant optipng pngcrush jpegoptim gifsicle libwebp-dev libheif-dev libjpeg-dev libpng-dev
+apt-get install build-essential webp pngquant optipng pngcrush jpegoptim gifsicle libwebp-dev libheif-dev libjpeg-dev libpng-dev libopenjp2-7-dev libjxr-dev
 wget http://www.jonof.id.au/files/kenutils/pngout-20200115-linux.tar.gz
 tar -xf pngout-20200115-linux.tar.gz
 rm pngout-20200115-linux.tar.gz
 cp pngout-20200115-linux/amd64/pngout /bin/pngout && rm -rf pngout-20200115-linux
 wget https://imagemagick.org/archive/ImageMagick.tar.gz
 tar -xzf ImageMagick.tar.gz
-rm -xzf ImageMagick.tar.gz
+rm ImageMagick.tar.gz
 cd ImageMagick*
 ./configure --with-modules
 make
@@ -21,6 +21,8 @@ phpize && ./configure
 make
 make install
 echo "extension=imagick.so" >> /etc/php/8.1/fpm/conf.d/20-imagic.ini
+cp /etc/php/8.1/fpm/conf.d/20-imagic.ini /etc/php/8.1/cli/conf.d/20-imagic.ini
 cd ..
 rm -rf imagick-*
 php -r "print_r(Imagick::queryFormats());"
+service php8.1-fpm restart
