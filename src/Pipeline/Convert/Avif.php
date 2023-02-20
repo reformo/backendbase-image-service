3baafe19b455751cb089232b9e5da6cbf8028a5b
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2023-02-20 19:00:49 +0300

    fix:alpha channel

--- src/Pipeline/Convert/Avif.php
+++ src/Pipeline/Convert/Avif.php
@@ -17,7 +17,7 @@ class Avif implements Converter
         $image = new Imagick($sourceFile);
         $image->setImageFormat('AVIF');
         $image->setOption('heic:lossless', 'true');
-        $image->setImageAlpha(100);
+        $image->setImageAlphaChannel(imagick::ALPHACHANNEL_ACTIVATE);
         $image->writeImage($newSourceFile);
         if ($newSourceFile !== $sourceFile) {
             unlink($sourceFile);
